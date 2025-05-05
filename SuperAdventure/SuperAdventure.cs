b268af197f3f32ceacd430d3295c39883405583f
Jordan Ferrer <jordanrcf25@gmail.com>
2025-05-05 02:40:46 -0700

    Lesson 03.1 – Building the first screen

---
+++ SuperAdventure/SuperAdventure.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SuperAdventure
+{
+    public partial class SuperAdventure : Form
+    {
+        public SuperAdventure()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            lblGold.Text = "123";
+        }
+
+        private void lblExperience_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblLevel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SuperAdventure_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
